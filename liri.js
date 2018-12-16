8cc87f377a9e493581b4ed63d0b44a399eb3f7a3
Vahe Minasyan (Red Door Collaborative LLC) <v-vamin@microsoft.com>
2018-12-15 16:59:41 -0800

    added error handler

--- liri.js
+++ liri.js
@@ -1,31 +1,10 @@
 require("dotenv").config();
 var keys = require("./keys");
 var moment = require('moment');
-const fs = require('fs');
+var fs = require('fs');
 
 var prompt = process.argv[2];
 var input = process.argv.slice(3).join(" ");
-
-// do-what-it-says
-if (prompt === "do-what-it-says") {
-
-    fs.readFile("random.txt", "utf8", function (error, data) {
-        if (error) throw error;
-        array = data.split(",");
-        prompt = array[0];
-        input = array[1];
-        if (prompt === "spotify-this-song")
-            spotify(input);
-
-        else if (prompt === "concert-this") {
-            concert(input);
-        }
-
-        else if (prompt === "movie-this") {
-            movie(input);
-        }
-    })
-}
 
 // spotify-this-song
 function spotify(input) {
@@ -61,30 +40,33 @@ function spotify(input) {
             if (err) {
                 return console.log('Error occurred: ' + err);
             }
-            for (var key in data) {
-                var track = data[key].items;
-                var showData = [];
-                for (var i = 0; i < track.length; i++) {
-                    var showDataObj = {
-                        ResultNumber: i + 1,
-                        Artist: track[i].album.artists[0].name,
-                        Song: track[i].name,
-                        Link: track[i].external_urls.spotify,
-                        Album: track[i].album.name
-                    }
-                    showData.push(showDataObj);
-                    //console.log(showData[i]);
-                    fs.appendFile('log.txt', JSON.stringify(showData[i]) + "\r\n-----------------\r\n", function (error) {
-                        if (error) { throw error }
-                    });
-                    console.log("\n-----------------\n")
-                    console.log(JSON.stringify(showData[i].ResultNumber))
-                    console.log("Artist(s): " + JSON.stringify(showData[i].Artist))
-                    console.log("Song: " + JSON.stringify(showData[i].Song))
-                    console.log("Link: " + JSON.stringify(showData[i].Link))
-                    console.log("Album: " + JSON.stringify(showData[i].Album))
+            if (JSON.stringify(data).length === 170) { console.log("We didn't found any result. Please try something else.") }
+            else {
+                for (var key in data) {
+                    var track = data[key].items;
+                    var showData = [];
+                    for (var i = 0; i < track.length; i++) {
+                        var showDataObj = {
+                            ResultNumber: i + 1,
+                            Artist: track[i].album.artists[0].name,
+                            Song: track[i].name,
+                            Link: track[i].external_urls.spotify,
+                            Album: track[i].album.name
+                        }
+                        showData.push(showDataObj);
+                        //console.log(showData[i]);
+                        fs.appendFile('log.txt', JSON.stringify(showData[i]) + "\r\n-----------------\r\n", function (error) {
+                            if (error) { throw error }
+                        });
+                        console.log("\n-----------------\n")
+                        console.log(JSON.stringify(showData[i].ResultNumber))
+                        console.log("Artist(s): " + JSON.stringify(showData[i].Artist))
+                        console.log("Song: " + JSON.stringify(showData[i].Song))
+                        console.log("Link: " + JSON.stringify(showData[i].Link))
+                        console.log("Album: " + JSON.stringify(showData[i].Album))
+                    };
                 };
-            };
+            }
         });
     }
 }
@@ -94,29 +76,34 @@ function concert(input) {
     var request = require('request');
     request("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp", function (error, response, body) {
         console.log('error:', error);
-        var data = JSON.parse(body);
-        var length;
-        if (data.length > 5) { length = 5 } else { length = data.length };
-        var showData = [];
-        for (var i = 0; i < length; i++) {
-            var showDataObj = {
-                ResultNumber: i + 1,
-                Artist: data[i].lineup[0],
-                Venue: data[i].venue.name,
-                Location: data[i].venue.city + ", " + data[i].venue.region + " " + data[i].venue.country,
-                Date: moment(data[i].datetime).format("MM/DD/YYYY")
-            }
-            showData.push(showDataObj);
-            //console.log(showData[i]);
-            fs.appendFile('log.txt', JSON.stringify(showData[i]) + "\r\n-----------------\r\n", function (error) {
-                if (error) { throw error }
-            });
-            console.log("\n-----------------\n");
-            console.log(JSON.stringify(showData[i].ResultNumber));
-            console.log("Artist: " + JSON.stringify(showData[i].Artist));
-            console.log("Venue: " + JSON.stringify(showData[i].Venue));
-            console.log("Location: " + JSON.stringify(showData[i].Location));
-            console.log("Date" + JSON.stringify(showData[i].Date))
+        if (JSON.parse(body.length) === 17) { console.log("We didn't found any result. Please try something else.") }
+
+        else {
+            var data = JSON.parse(body);
+            var length;
+            if (data.length > 5) { length = 5 }
+            else { length = data.length };
+            var showData = [];
+            for (var i = 0; i < length; i++) {
+                var showDataObj = {
+                    ResultNumber: i + 1,
+                    Artist: data[i].lineup[0],
+                    Venue: data[i].venue.name,
+                    Location: data[i].venue.city + ", " + data[i].venue.region + " " + data[i].venue.country,
+                    Date: moment(data[i].datetime).format("MM/DD/YYYY")
+                }
+                showData.push(showDataObj);
+                //console.log(showData[i]);
+                fs.appendFile('log.txt', JSON.stringify(showData[i]) + "\r\n-----------------\r\n", function (error) {
+                    if (error) { throw error }
+                });
+                console.log("\n-----------------\n");
+                console.log(JSON.stringify(showData[i].ResultNumber));
+                console.log("Artist: " + JSON.stringify(showData[i].Artist));
+                console.log("Venue: " + JSON.stringify(showData[i].Venue));
+                console.log("Location: " + JSON.stringify(showData[i].Location));
+                console.log("Date" + JSON.stringify(showData[i].Date))
+            };
         };
     });
 }
@@ -130,29 +117,55 @@ function movie(input) {
     var request = require('request');
     request("http://www.omdbapi.com/?apikey=trilogy&t=" + input, function (error, response, body) {
         console.log('error:', error);
-        var result = JSON.parse(body);
-        var rating = "";
-        //console.log(result);
-        if (result.Ratings[1] === undefined) {
-            rating = "RT Rating: N/A"
+        console.log("Hello " + JSON.parse(body.length));
+        if (JSON.parse(body.length) === 47) { console.log("We didn't found any result. Please try something else.") }
+
+        else {
+            var result = JSON.parse(body);
+            var rating = "";
+            //console.log(result);
+            if (result.Ratings[1] === undefined) {
+                rating = "RT Rating: N/A"
+            }
+            else { rating = "RT Rating: " + result.Ratings[1].Value };
+            var showData = [
+                "Title: " + result.Title,
+                "Year: " + result.Year,
+                "IMBD Rating: " + result.imdbRating,
+                "Country: " + result.Country,
+                "Language: " + result.Language,
+                "Plot: " + result.Plot,
+                "Actors: " + result.Actors,
+                "Rating: " + rating
+            ].join("\r\n")
+            fs.appendFile('log.txt', showData + "\r\n-----------------\r\n", function (error) {
+                if (error) { throw error }
+                console.log("\n-----------------\n")
+                console.log(showData)
+            });
         }
-        else { rating = "RT Rating: " + result.Ratings[1].Value };
-        var showData = [
-            "Title: " + result.Title,
-            "Year: " + result.Year,
-            "IMBD Rating: " + result.imdbRating,
-            "Country: " + result.Country,
-            "Language: " + result.Language,
-            "Plot: " + result.Plot,
-            "Actors: " + result.Actors,
-            "Rating: " + rating
-        ].join("\r\n")
-        fs.appendFile('log.txt', showData + "\r\n-----------------\r\n", function (error) {
-            if (error) { throw error }
-            console.log("\n-----------------\n")
-            console.log(showData)
-        });
     });
+}
+
+// do-what-it-says
+if (prompt === "do-what-it-says") {
+
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) throw error;
+        array = data.split(",");
+        prompt = array[0];
+        input = array[1];
+        if (prompt === "spotify-this-song")
+            spotify(input);
+
+        else if (prompt === "concert-this") {
+            concert(input);
+        }
+
+        else if (prompt === "movie-this") {
+            movie(input);
+        }
+    })
 }
 
 if (prompt === "spotify-this-song")
